ad3fad8ae1c708c386371ac8681bd18a763baba4
Geo-hzr <103549146+Geo-hzr@users.noreply.github.com>
2026-01-15 14:06:45 -0500

    Update scene_classification_network.py

--- scene_classification_network.py
+++ scene_classification_network.py
@@ -3,135 +3,132 @@ from tensorflow.keras import  Model
 from tensorflow.keras.applications import *
 from spektral.layers import *
 
-def pretrained_dense_net(inputs, num_classes):
+def build_pointnet(inputs, normal_vec, num_channels):
 
-    x = inputs
+    # Rotation transformation
+    x = Conv1D(1 * num_channels, 1)(inputs)
+    x = BatchNormalization()(x)
+    x = LeakyReLU(0.01)(x)
+    x = Conv1D(2 * num_channels, 1)(x)
+    x = BatchNormalization()(x)
+    x = LeakyReLU(0.01)(x)
+    x = Conv1D(8 * num_channels, 1)(x)
+    x = BatchNormalization()(x)
+    x = LeakyReLU(0.01)(x)
+    x = GlobalMaxPool1D()(x)
+    x = Dense(4 * num_channels, activation='relu')(x)
+    x = Dense(2 * num_channels, activation='relu')(x)
+    x = Dense(3 * 3, weights=[np.zeros([2 * num_channels, 3 * 3]), np.eye(3).flatten().astype(np.float32)])(x)
+    r_mat = Reshape((3, 3))(x)
+
+    r = Dot(axes=2)([inputs, r_mat])
+    r = Conv1D(1 * num_channels, 1)(r)
+    r = BatchNormalization()(r)
+    r = LeakyReLU(0.01)(r)
+    r = Conv1D(1 * num_channels - 3, 1)(r)
+    r = BatchNormalization()(r)
+    r = LeakyReLU(0.01)(r)
+
+    # Feature transformation
+    r = Concatenate()([r, normal_vec])
+    x = Conv1D(1 * num_channels, 1)(r)
+    x = BatchNormalization()(x)
+    x = LeakyReLU(0.01)(x)
+    x = Conv1D(2 * num_channels, 1)(x)
+    x = BatchNormalization()(x)
+    x = LeakyReLU(0.01)(x)
+    x = Conv1D(8 * num_channels, 1)(x)
+    x = BatchNormalization()(x)
+    x = LeakyReLU(0.01)(x)
+    x = GlobalMaxPool1D()(x)
+    x = Dense(4 * num_channels, activation='relu')(x)
+    x = Dense(2 * num_channels, activation='relu')(x)
+    x = Dense((1 * num_channels) * (1 * num_channels), weights=[np.zeros([(2 * num_channels), (1 * num_channels) * (1 * num_channels)]), np.eye((1 * num_channels)).flatten().astype(np.float32)])(x)
+    f_mat = Reshape(((1 * num_channels), (1 * num_channels)))(x)
+
+    f = Dot(axes=2)([r, f_mat])
+    f = Conv1D(1 * num_channels, 1)(f)
+    f = BatchNormalization()(f)
+    f = LeakyReLU(0.01)(f)
+    f = Conv1D(2 * num_channels, 1)(f)
+    f = BatchNormalization()(f)
+    f = LeakyReLU(0.01)(f)
+    f = Conv1D(16 * num_channels, 1)(f)
+    f = BatchNormalization()(f)
+    f = LeakyReLU(0.01)(f)
+
+    x = GlobalMaxPool1D()(f)
+
+    return x
+
+def build_densenet(inputs, num_classes):
 
     model_path = r'pretrained_model/densenet121_weights_tf_dim_ordering_tf_kernels_notop.h5'
-    DN = DenseNet121(
+    dn121 = DenseNet121(
         weights=model_path,
         include_top=False,
-        input_tensor=x,
+        input_tensor=inputs,
         classes=num_classes)
 
-    DN.trainable = False#True
-    x = DN(x)
+    dn121.trainable = False
+
+    x = dn121(inputs)
     x = GlobalAvgPool2D()(x)
 
     return x
 
-def graph_attention_net(input_x, input_a, input_a_n, channels, attn_heads, num_nodes):
+def build_gan(inputs, adj_mat, adj_mat_normalized, num_channels, num_heads, num_nodes):
 
-    x = BatchNormalization()(input_x)
-    x = GATConv(channels=channels, attn_heads=attn_heads)([x, input_a])
+    x = BatchNormalization()(inputs)
+    x = GATConv(channels=num_channels, attn_heads=num_heads)([x, adj_mat])
     x = LeakyReLU(0.01)(x)
     x = BatchNormalization()(x)
-    x, a = DMoNPool(k=num_nodes // 2, mlp_hidden=[num_nodes])([x, input_a_n])
+    x, _ = DMoNPool(k=num_nodes // 2, mlp_hidden=[num_nodes])([x, adj_mat_normalized])
     x = LeakyReLU(0.01)(x)
     x = GlobalMaxPool1D()(x)
 
     return x
 
-def modified_point_net(input_data, normal_vector, channels):
+def build_scn(num_points=2048,
+              num_features=3,
+              img_height=128,
+              img_width=512,
+              num_nodes=1024,
+              num_channels=1,
+              num_classes=2):
 
-    # rotation transformation
-    x = Conv1D(1 * channels, 1)(input_data)
-    x = BatchNormalization()(x)
-    x = ReLU()(x)
-    x = Conv1D(2 * channels, 1)(x)
-    x = BatchNormalization()(x)
-    x = ReLU()(x)
-    x = Conv1D(8 * channels, 1)(x)
-    x = BatchNormalization()(x)
-    x = ReLU()(x)
-    x = GlobalMaxPool1D()(x)
-    x = Dense(4 * channels, activation='relu')(x)
-    x = Dense(2 * channels, activation='relu')(x)
-    x = Dense(3 * 3, weights=[np.zeros([2 * channels, 3 * 3]), np.eye(3).flatten().astype(np.float32)])(x)
-    input_T = Reshape((3, 3))(x)
+    pcd = Input(shape=(num_points, num_features))
 
-    g = Dot(axes=2)([input_data, input_T])
-    g = Conv1D(1 * channels, 1)(g)
-    g = BatchNormalization()(g)
-    g = ReLU()(g)
-    g = Conv1D(1 * channels - 3, 1)(g)
-    g = BatchNormalization()(g)
-    g = ReLU()(g)
+    normal_vec = Input(shape=(num_points, num_features))
 
-    # feature transformation
-    g = Concatenate()([g, normal_vector])
-    f = Conv1D(1 * channels, 1)(g)
-    f = BatchNormalization()(f)
-    f = ReLU()(f)
-    f = Conv1D(2 * channels, 1)(f)
-    f = BatchNormalization()(f)
-    f = ReLU()(f)
-    f = Conv1D(8 * channels, 1)(f)
-    f = BatchNormalization()(f)
-    f = ReLU()(f)
-    f = GlobalMaxPool1D()(f)
-    f = Dense(4 * channels, activation='relu')(f)
-    f = Dense(2 * channels, activation='relu')(f)
-    f = Dense((1 * channels) * (1 * channels), weights=[np.zeros([(2 * channels), (1 * channels) * (1 * channels)]), np.eye((1 * channels)).flatten().astype(np.float32)])(f)
-    feature_T = Reshape(((1 * channels), (1 * channels)))(f)
+    img = Input(shape=(img_height, img_width, 3))
 
-    g = Dot(axes=2)([g, feature_T])
-    g = Conv1D(1 * channels, 1)(g)
-    g = BatchNormalization()(g)
-    g = ReLU()(g)
-    g = Conv1D(2 * channels, 1)(g)
-    g = BatchNormalization()(g)
-    g = ReLU()(g)
-    g = Conv1D(16 * channels, 1)(g)
-    g = BatchNormalization()(g)
-    g = ReLU()(g)
+    adj_mat = Input(shape=(num_nodes, num_nodes))
 
-    global_feature = GlobalMaxPool1D()(g)
+    adj_mat_normalized = Input(shape=(num_nodes, num_nodes))
 
-    return global_feature
+    dc_vec = Input(shape=(num_nodes, num_channels))
 
-def scene_classification_net(num_points=2048,
-                             num_features=3,
-                             img_width=512,
-                             img_height=128,
-                             num_nodes=1024,
-                             num_channels=1,
-                             num_classes=2):
+    f1 = build_pointnet(pcd, normal_vec, num_channels=64) # 16
+    f2 = build_densenet(img, num_classes)
+    f3 = build_gan(dc_vec, adj_mat, adj_mat_normalized,
+                   num_channels=64, num_heads=16, num_nodes=num_nodes) # 32 8
 
-    input_3d_point_cloud = Input(shape=(num_points, num_features))
+    x = Concatenate()([f1, f2, f3])
 
-    input_normal_vector = Input(shape=(num_points, num_features))
+    weights = Dense(x.shape[-1] // 2, activation='relu')(x)
+    weights = Dense(x.shape[-1], activation='sigmoid')(weights)
 
-    input_ad_matrix = Input(shape=(num_nodes, num_nodes))
-
-    input_n_ad_matrix = Input(shape=(num_nodes, num_nodes))
-
-    input_dc_vector = Input(shape=(num_nodes, num_channels))
-
-    input_2d_image = Input(shape=(img_height, img_width, 3))
-
-    h1 = modified_point_net(input_3d_point_cloud, input_normal_vector, channels=64)
-    h2 = pretrained_dense_net(input_2d_image, num_classes)
-    h3 = graph_attention_net(input_dc_vector, input_ad_matrix, input_n_ad_matrix,
-                                   channels=64, attn_heads=16,num_nodes=num_nodes)
-
-    x = Concatenate()([h1, h2, h3])
-
-    channel_num = x.shape[-1]
-
-    weight = Dense(channel_num // 2, activation='relu')(x)
-    weight = Dense(channel_num, activation='sigmoid')(weight)
-
-    x = Multiply()([x, weight])
+    x = Multiply()([x, weights])
     x = Dense(1024, activation='relu')(x)
     x = Dense(512, activation='relu')(x)
 
-    prediction = Dense(1, activation='sigmoid')(x)
+    output = Dense(1, activation='sigmoid')(x)
 
     model = Model(
-        inputs=[input_3d_point_cloud, input_normal_vector, input_ad_matrix, input_dc_vector, input_2d_image, input_n_ad_matrix],
-        outputs=prediction)
+        inputs=[pcd, normal_vec, img, adj_mat, adj_mat_normalized, dc_vec],
+        outputs=output)
 
-    print(r'model initialization done.')
+    print(r'Model initialization done.')
 
     return model
