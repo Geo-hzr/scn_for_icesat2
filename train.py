bd1446b2c7697c5ee7d9980a7da760bf0be6662a
Geo-hzr <103549146+Geo-hzr@users.noreply.github.com>
2026-01-21 22:50:05 -0500

    Update train.py

--- train.py
+++ train.py
@@ -26,7 +26,7 @@ def train_model():
 
     model.compile(optimizer=optimizers.Adam(learning_rate=1e-4, decay=1e-8, clipvalue=1.0), loss=losses.binary_crossentropy, metrics=[metrics.binary_accuracy])
 
-    pcd_lst, normal_vec_lst, img_lst, adj_mat_lst, adj_mat_normalized_lst, dc_vec_lst, label_lst = feature_augmentation.construct_feature_space()
+    pcd_lst, normal_vec_lst, img_lst, adj_mat_lst, normalized_adj_mat_lst, dc_vec_lst, label_lst = feature_augmentation.construct_feature_space()
 
     idx_lst = [i for i in range(len(label_lst))]
     np.random.shuffle(idx_lst)
@@ -35,26 +35,23 @@ def train_model():
     normal_vec_lst = np.array(normal_vec_lst)[idx_lst]
     img_lst = np.array(img_lst)[idx_lst] / 255.
     adj_mat_lst = np.array(adj_mat_lst)[idx_lst]
-    adj_mat_normalized_lst = np.array(adj_mat_normalized_lst)[idx_lst]
+    normalized_adj_mat_lst = np.array(normalized_adj_mat_lst)[idx_lst]
     dc_vec_lst = np.array(dc_vec_lst)[idx_lst]
     label_lst = np.array(label_lst)[idx_lst]
 
-    print(pcd_lst.shape, normal_vec_lst.shape, img_lst.shape, adj_mat_lst.shape, dc_vec_lst.shape, label_lst.shape)
-
     callback = callbacks.EarlyStopping(monitor='val_loss', patience=PATIENCE, mode='min')
 
-    history = model.fit([pcd_lst, normal_vec_lst, img_lst, adj_mat_lst, adj_mat_normalized_lst, dc_vec_lst], label_lst, epochs=20, batch_size=32, validation_split=VALIDATION_SPLIT, callbacks=[callback])
-    
+    history = model.fit([pcd_lst, normal_vec_lst, img_lst, adj_mat_lst, normalized_adj_mat_lst, dc_vec_lst], label_lst, epochs=20, batch_size=32, validation_split=VALIDATION_SPLIT, callbacks=[callback])
+
     acc = history.history['binary_accuracy']
     loss = history.history['loss']
-    epochs = range(1, len(acc) + 1)
     plt.subplot(1, 2, 1)
-    plt.scatter(epochs, acc, label='Training accuracy')
+    plt.scatter(range(1, len(acc) + 1), acc, label='Training accuracy')
     plt.xlabel('Epochs')
     plt.ylabel('Accuracy')
     plt.grid(color='black', linestyle='--')
     plt.subplot(1, 2, 2)
-    plt.scatter(epochs, loss, label='Training loss')
+    plt.scatter(range(1, len(acc) + 1), loss, label='Training loss')
     plt.xlabel('Epochs')
     plt.ylabel('Loss')
     plt.grid(color='black', linestyle='--')
